642496a8c8fce16093202bd19e9a013e5735e552
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-07-12 09:48:42 -0700

    initialized spring project

---
+++ src/main/java/cleanplate/cleanplatehombres/CleanplatehombresApplication.java
@@ -0,0 +1,13 @@
+package cleanplate.cleanplatehombres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanplatehombresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanplatehombresApplication.class, args);
+    }
+
+}
