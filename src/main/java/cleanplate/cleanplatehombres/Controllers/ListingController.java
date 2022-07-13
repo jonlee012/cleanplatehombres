f5f1f6c8bb3b71a1ea13c66cb5370b965a83ed02
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-07-13 12:10:37 -0700

    added edit controller get and post mapping. edited listing index and verified listings are displaying.

--- src/main/java/cleanplate/cleanplatehombres/Controllers/ListingController.java
+++ src/main/java/cleanplate/cleanplatehombres/Controllers/ListingController.java
@@ -4,9 +4,7 @@ import cleanplate.cleanplatehombres.Repositories.ListingRepository;
 import cleanplate.cleanplatehombres.models.Listing;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ListingController {
@@ -17,12 +15,16 @@ public class ListingController {
         this.listingRepository = listingRepository;
     }
 
+
+    //listing index controller
     @GetMapping("/listings")
     public String index(Model model) {
         model.addAttribute("listings", listingRepository.findAll());
         return "listings/index";
     }
 
+
+   //create controller
     @GetMapping("/listings/create")
     public String create(Model model) {
         model.addAttribute("listing", new Listing());
@@ -39,11 +41,28 @@ public class ListingController {
         return "redirect:/listings";
     }
 
+   //show controller
     @GetMapping("listings/show")
     public String showPage() {
         return "listings/show";
     }
 
+    //edit controller
+    @GetMapping("listings/edit/{id}")
+    public String editListing(@PathVariable long id, Model model) {
+        Listing editListing = listingRepository.getById(id);
+        model.addAttribute("listingToEdit", editListing);
+        return "listings/edit";
+    }
 
-
+//    @PostMapping("listings/edit/{id}")
+//    public String saveEditedListing(@PathVariable long id,
+//                                    @RequestParam(name="donation_description") String donation_description){
+//        Listing listingToEdit = listingRepository.getById(id);
+//        listingToEdit.setDonation_description(listingDonation_description);
+//        listingToEdit.setFood_name(listingFood_name);
+//        listingRepository.save(listingToEdit);
+//        return "redirect:/listings";
+//    }
+//
 }
