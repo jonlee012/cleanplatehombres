bf743ca3ead931a3b4fadd1cabd233f4ffed40bc
jonlee012 <jonlee86.jl@gmail.com>
2022-07-14 16:27:23 -0500

    jon updates to map

--- src/main/resources/static/weather_map.js
+++ src/main/resources/static/weather_map.js
@@ -1,136 +1,9 @@
-// "use strict"
 //
-// // setting up global variables to call the data from Weather Map
-// var longitude = -98.7320
-// var latitude = 29.7947
-//
-// // Calling initial getData function
-// getData();
-//
-// // ------- Query the Weather Map API ----------------------------------------------
-//
-// // String and Object Method:
-// // Using this method for the sake of readability
-// // Excluding minutely and hourly.
-// // Changing kelvins to imperial: Fahrenheit
-// // Defining the 'get' request as a function 'getData' in order to call it multiple times
-// function getData() {
-//     $.get("https://api.openweathermap.org/data/2.5/onecall", {
-//         APPID: OPEN_WEATHER_APPID,
-//         lat: latitude,
-//         lon: longitude,
-//         units: "imperial",
-//         exclude: "minutely, hourly"
-//     }).done(function (data) {
-//         handleResponse(data)
-//     });
-//
-// }
+// // new edits
 //
 //
-// // Setting up handle response to iterate through the returned data and populate the html:
-// function handleResponse(data) {
-//     let days = data.daily;
-//     let html = "";
-//     for (var i = 0; i < 5; i++) {
-//         let date = dateMaker(i);
-//         let iconCode = days[i].weather[0].icon;
-//         let tempHigh = Math.round(days[i].temp.max);
-//         let tempLow = Math.round(days[i].temp.min);
-//         let daysHum = days[i].humidity;
-//         let dayPress = days[i].pressure;
-//         let dayWind = days[i].wind_speed;
-//         let description = days[i].weather[0].description;
-// // Embedding into the div.card element using string method:
-//         let itemHtml = "<div style='align-items: center; margin-bottom: 50px' class='card col-2' style='width: 17rem'>"
-//         itemHtml += '<span class="date-text">' + date + '</span>';
-//         itemHtml += "<img style='height: 50px; width: 50px;' src='http://openweathermap.org/img/w/" + iconCode + ".png'>" // Refactored the image names of the local icons to work with this
-//         itemHtml += '<h5 class="highText">' + 'High ' + tempHigh + '</h5>';
-//         itemHtml += '<h5 class="lowText">' + 'Low ' + tempLow + '</h5>';
-//         itemHtml += '<p class=“humid” style="font-size:12px; margin: 0">' + 'Humidity: ' + daysHum + '</p>';
-//         itemHtml += '<p class=“pressure" style="font-size:12px; margin: 0">' + 'Pressure: ' + dayPress + '</p>';
-//         itemHtml += '<p class=“wind” style="font-size:12px; margin: 0">' + 'Wind: ' + dayWind + ' mph' + '</p>';
-//         itemHtml += '<p class="card-footer my-1">' + description + '</p>';
-//         itemHtml += '</div>';
-//         html += itemHtml;
-//     }
-//     $('#insertWeatherBoxes').html(html);
+// "use strict";
 //
-//     let coordinates = {
-//         lng: data.lon,
-//         lat: data.lat
-//     }
-//
-//     let myLocation = reverseGeocode(coordinates, MAPBOX_KEY);
-//     myLocation.then(function(data) {
-//         $('#locationPrinted').text(data);});
-//
-//     //------- Map -----------------------------------------------------
-//
-//     mapboxgl.accessToken = MAPBOX_KEY
-//     const map = new mapboxgl.Map({
-//         container: 'map', // container ID
-//         style: 'mapbox://styles/mapbox/light-v10', // style URL
-//         center: [longitude, latitude], // starting position [lng, lat]
-//         zoom: 9 // starting zoom
-//     });
-//
-// // Map Nav Controls
-//     map.addControl(new mapboxgl.NavigationControl());
-//
-// //Starting Draggable Marker (default point)
-//     var marker = new mapboxgl.Marker({
-//         draggable: true
-//     })
-//         .setLngLat([longitude, latitude])
-//         .addTo(map);
-//
-//     console.log(marker.getLngLat())
-//
-// // Adding functionality to draggable marker
-//
-//     function onDragEnd() {
-//         var lngLat = marker.getLngLat();
-//         longitude = lngLat.lng;
-//         latitude = lngLat.lat;
-//         getData();
-//     }
-//     marker.on('dragend', onDragEnd);
-// }
-//
-// // Trying to manipulate the date() object to work with the weather cards.
-// // Pass the dateMaker function the index number (i) in the handleResponse.
-// // The setDate() method sets the day of the Date object relative
-// // to the beginning of the currently set month. -MDN
-// // The getDate() method returns the day of the month for the specified
-// // date according to local time. -MDN
-//
-// function dateMaker(num) {
-//     let date = new Date();
-//     date.setDate(date.getDate() + num)
-//     return date.toDateString().slice(0, 10);
-// }
-//
-// //------- Search by City geocode ----------------------------------------
-//
-// $(".btn").click(function (e) {
-//     e.preventDefault()
-//     let searchInput = $("#input").val();
-//     geocode(searchInput, MAPBOX_KEY).then(function (data) {
-//         longitude = data[0];
-//         latitude = data[1];
-//         getData();
-//     })
-// })
-
-
-
-
-// new edits
-
-
-"use strict";
-
 function geocode(search, token) {
     var baseUrl = 'https://api.mapbox.com';
     var endPoint = '/geocoding/v5/mapbox.places/';
@@ -155,15 +28,15 @@ function geocode(search, token) {
 //             return data.features[2].place_name;
 //         });
 // }
-var longitude = 98.4946
-var latitude = 29.4252
+// var longitude = 98.4946
+// var latitude = 29.4252
 
 
 mapboxgl.accessToken = MAPBOX_KEY
 var map = new mapboxgl.Map({
     container: 'map', // HTML container id
     style: 'mapbox://styles/mapbox/streets-v9', // style URL
-    center: [-21.9270884, 64.1436456], // starting position as [lng, lat]
+    center: [-98.4916, 29.4252],
     zoom: 13
 });
 // Map Nav Controls
@@ -173,16 +46,28 @@ map.addControl(new mapboxgl.NavigationControl());
 var marker = new mapboxgl.Marker({
     draggable: true
 })
-    .setLngLat([longitude, latitude])
+    .setLngLat([-98.4916, 29.4252])
     .addTo(map);
 
+// starting point for weather
+var weatherOptions = {
+    lat: 29.4241,
+    lon: -98.4936,
+    appid: "8dba1fe72474760cbb96da7c28d67b5e",
+    units: 'imperial'
+};
 
 function onDragEnd() {
     var lngLat = marker.getLngLat();
-    longitude = lngLat.lng;
-    latitude = lngLat.lat;
-    getData();
+    var lat = lngLat.lat
+    var lng = lngLat.lng
+
+    weatherOptions.lng = lng
+    weatherOptions.lat = lat
+    // renderWeather()
+
 }
+
 marker.on('dragend', onDragEnd);
 
 $(".btn").click(function (e) {
@@ -194,18 +79,10 @@ $(".btn").click(function (e) {
         getData();
     })
 })
-// var map = new mapboxgl.Map({
-//     container: 'map', // HTML container id
-//     style: 'mapbox://styles/mapbox/streets-v9', // style URL
-//     center: [-21.92661562, 64.14356426], // starting position as [lng, lat]
-//     zoom: 13
-// });
-//
-// var popup = new mapboxgl.Popup()
-//     .setHTML('<h3>Reykjavik Roasters</h3><p>A good coffee shop</p>');
-//
-// var marker = new mapboxgl.Marker()
-//     .setLngLat([-21.92661562, 64.14356426])
-//     .setPopup(popup)
-//     .addTo(map);
-//
+
+
+
+
+
+
+
