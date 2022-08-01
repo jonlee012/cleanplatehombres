ff6d7c5b1638e555bca88025a8347a5d4fd45066
Derek <99042983+derekberkley@users.noreply.github.com>
2022-08-01 14:58:07 -0500

    trying things with index

--- src/main/java/cleanplate/cleanplatehombres/Controllers/HomeController.java
+++ src/main/java/cleanplate/cleanplatehombres/Controllers/HomeController.java
@@ -1,13 +1,25 @@
 package cleanplate.cleanplatehombres.Controllers;
 
+import cleanplate.cleanplatehombres.Repositories.ListingRepository;
+import cleanplate.cleanplatehombres.Repositories.OrganizationRepository;
+import cleanplate.cleanplatehombres.Repositories.UserRepository;
+import org.apache.tomcat.util.modeler.BaseAttributeFilter;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+//    private final OrganizationRepository organizationRepository;
+//
+//    public HomeController (OrganizationRepository organizationRepository) {
+//        this.organizationRepository = organizationRepository;
+//    }
+
     @GetMapping("/index")
-    public String index(){
+    public String index(Model model){
+//        model.addAttribute("organizations", organizationRepository.findAll());
         return "index";
     }
 
